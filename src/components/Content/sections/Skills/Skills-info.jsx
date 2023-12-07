e05b15e9531f4a55030a302a5064995072374927
Edy Rodriguez <zeronoaz@gmail.com>
2023-12-07 14:28:16 -0600

    feat: few changes before total change in tailwind

---
+++ src/components/Content/sections/Skills/Skills-info.jsx
@@ -0,0 +1,78 @@
+import { SiTypescript } from "react-icons/si";
+import { GrReactjs } from "react-icons/gr";
+import { FaNode } from "react-icons/fa";
+import { FaAngular } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { DiDjango } from "react-icons/di";
+import { FaBootstrap } from "react-icons/fa";
+import { IoLogoFirebase } from "react-icons/io5";
+import { FaGit } from "react-icons/fa";
+import { SiJira } from "react-icons/si";
+import { DiMongodb } from "react-icons/di";
+import { TbBrandReactNative } from "react-icons/tb";
+
+const skillsInfo = [
+  {
+    id: 0,
+    title: "Typescript",
+    icon: <SiTypescript />
+  },
+  {
+    id: 1,
+    title: "React",
+    icon: <GrReactjs/>
+  },
+  {
+    id: 2,
+    title: "React Native",
+    icon: <TbBrandReactNative/>
+  },
+  {
+    id: 3,
+    title: "Angular",
+    icon: <FaAngular/>
+  },
+  {
+    id: 4,
+    title: "Django",
+    icon:<DiDjango/>
+  },
+  {
+    id: 5,
+    title: "Node",
+    icon: <FaNode/>
+  },
+  {
+    id: 6,
+    title: "Bootstrap",
+    icon: <FaBootstrap/>
+  },
+  {
+    id: 7,
+    title: "Tailwind",
+    icon: <SiTailwindcss/>
+  },
+  {
+    id: 8,
+    title: "Firebase",
+    icon: <IoLogoFirebase/>
+  },
+  {
+    id: 9,
+    title: "Git",
+    icon: <FaGit/>
+  },
+  {
+    id: 10,
+    title: "Jira",
+    icon: <SiJira/>
+  },
+  {
+    id:11,
+    title: "Mongo DB",
+    icon: <DiMongodb/>
+  }
+];
+
+
+export default skillsInfo;
--- src/components/Content/sections/Skills/Skills.jsx
+++ src/components/Content/sections/Skills/Skills.jsx
@@ -1,23 +1,26 @@
 import useTranslation from "../../../customHooks/translations";
-import './Skills.css';
-import skillsInfo  from './Skills-info';
+import "./Skills.css";
+import skillsInfo from "./Skills-info";
 export default function Skills() {
-    const translation = useTranslation();
-    return (
-        <section className="skills">
-            <h1>{translation.skills.title}</h1>
-            <div className="skill-container">
-                 <ul className="skill-list">
-                    {
-                        skillsInfo.map((skill) =>
-                                 
-                                <li key={skill.id} className="skill">
-                                    {skill.title}
-                                </li> 
-                    )
-                }
-                 </ul>
-            </div>
-        </section>
-    );
+  const translation = useTranslation();
+  return (
+    <section className="skills">
+      <h1>{translation.skills.title}</h1>
+      <div className="skill-container">
+        <ul className="skill-list">
+          {skillsInfo.map((skill) => (
+            <li key={skill.id} className="skill">
+              <a
+                href="https://www.linkedin.com/in/edgar-fernando-rodriguez-rangel-261824160/"
+                className="social-links"
+                target="_blank"
+                rel="noreferrer">
+                {skill.icon}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
